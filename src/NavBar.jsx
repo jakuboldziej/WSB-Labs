51564f63ffeb012c1d460b3018e20c1b067f2fe1
jakuboldziej <jakub.oldziej@wp.pl>
2025-07-02 18:33:37 +0200

    navbar design b

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     <div className="navbar-height flex items-center gap-4 px-6">
       <Link to="/" className="hover:text-gray-700">Home</Link>
       <Link to="wsb" className="hover:text-gray-700">WSB</Link>
-      <Link to="nicosc" className="hover:text-gray-700">Nicosc</Link>
+      <Link to="cos" className="hover:text-gray-700">cos</Link>
     </div>
   )
 }
